266e4456bdc07b7ab2f044f11eb4d602e31bcd7c
hubert1217 <hubert.roj@gmail.com>
2021-03-05 13:34:46 +0100

    Dodaj pliki projektów.

---
+++ GameShop/Models/Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GameShop.Models
+{
+    public class Game
+    {
+        public int GameId { get; set; }
+        public int GenreId { get; set; }
+        public string GameTitle { get; set; }
+        public string GameStudio { get; set; }
+        public string Platform { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public string CovertFileName { get; set; }
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public bool IsBestseller { get; set; }
+
+        public bool IsHidden { get; set; }
+
+
+        public virtual Genre Genre { get; set; }
+
+    }
+}
---
+++ GameShop/Models/Genre.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace GameShop.Models
+{
+    public class Genre
+    {
+        public int GenreId { get; set; }
+        public string GenreName { get; set; }
+        public string GenreDescription { get; set; }
+        public string IconFilename { get; set; }
+
+
+        public ICollection<Game> Games { get; set; }
+
+    }
+}
